6ae644c0c2684621ab6f9f4400839a04f2a07471
Anurag Sarkar <sarkaranurag.anjankumar2015@vit.ac.in>
2018-03-28 19:44:37 +0530

    lala

---
+++ dev/gui.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import subprocess
+
+class Application(Frame):
+
+    def upload(self):
+        val = "lala"
+        subprocess.run(["git", "add", "."], shell=True)
+        subprocess.run(["git", "commit", "-m", val], shell=True)
+        subprocess.run(["git", "push"])
+        # return
+    def download(self):
+        subprocess.run(["git","pull"])
+        # return
+
+    def createWidgets(self):
+        # upload button
+        self.UPLOAD = Button(self)
+        self.UPLOAD["text"] = "UPLOAD"
+        self.UPLOAD["command"] =  self.upload
+        self.UPLOAD.pack({"side": "left"})
+
+        # download button 
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "DOWNLOAD",
+        self.hi_there["command"] = self.download
+        self.hi_there.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
